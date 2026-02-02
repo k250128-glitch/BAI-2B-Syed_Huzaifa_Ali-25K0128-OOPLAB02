4d1bc80a8cb4af0c837d823c5c3ccb7cbf4b6141
Syed Huzaifa Ali <k250128@nu.edu.pk>
2026-02-02 22:31:39 +0500

    Implement Circle class with area, diameter, and circumference

---
+++ q02.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+class Circle {
+private:
+    double radius;
+    double pi = 3.14159;
+
+public:
+    void setRadius(double r) {
+        radius = r;
+    }
+
+    double getRadius() {
+        return radius;
+    }
+
+    double getArea() {
+        return pi * radius * radius;
+    }
+
+    double getDiameter() {
+        return radius * 2;
+    }
+
+    double getCircumference() {
+        return 2 * pi * radius;
+    }
+};
+
+int main() {
+    Circle c;
+    double radius;
+
+    cout << "Enter the radius: ";
+    cin >> radius;
+
+    c.setRadius(radius);
+
+    cout << "Area: " << c.getArea() << endl;
+    cout << "Diameter: " << c.getDiameter() << endl;
+    cout << "Circumference: " << c.getCircumference() << endl;
+
+    return 0;
+}
